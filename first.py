cc812e5fff8f7908e48da2cf67cde0708becbe81
Sasha <alexa30che@gmail.com>
2022-11-13 12:30:09 +0200

    first.py

--- first.py
+++ first.py
@@ -6,15 +6,24 @@ def divider(a, b):
         raise ValueError
     if b > 100:
         raise IndexError
+    if b == 0:
+        raise ZeroDivisionError
+    if type(a) == "str" or type(a) == 'list':
+        raise TypeError
     return a/b
 
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+data = {10: 2, 2: 5, "123": 4, 18: 0, '[]': 15, 8: 4}
 for key in data:
     try:
         res = divider(key, data[key])
         result.append(res)
-    except :
+    except TypeError:
+        print('Dictionary Error')
+    except ValueError:
+        print('Value Error')
+    except ZeroDivisionError:
+        print("You can't divide by zero")
 
 
 print(result)
